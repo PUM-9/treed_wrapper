a9987e83be2d29814520c3c4911331beea4a4692
Martin <martin95lundberg@gmail.com>
2017-05-10 14:41:44 +0200

    Refactored args parsing

--- src/treed_wrapper.py
+++ src/treed_wrapper.py
@@ -4,6 +4,28 @@ import argparse
 import subprocess
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rotation_scans', '-r', type=int, help='the number of scans to take in rotation axis')
+    parser.add_argument('--curve_scans', '-c', type=int, help='the number of scans to take in curve axis')
+    args = parser.parse_args()
+
+    if args.rotation_scans:
+        rotation_angle = 360 / args.rotation_scans
+        if not rotation_angle:
+            rotation_angle = 1
+    else:
+        rotation_angle = 360
+
+    if args.curve_scans:
+        curve_angle = 100 / args.curve_scans
+        if not curve_angle:
+            curve_angle = 1
+    else:
+        curve_angle = 100
+
+    return rotation_angle, curve_angle
+
 def run_treed_scan(filename, rotation, curve):
     rotation_command = ['treed', 'set', '--table-rotation', str(rotation)]
     curve_command = ['treed', 'set', '--table-curve', str(curve)]
@@ -28,25 +50,8 @@ def run_filter(filename, rotation, curve):
         
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--rotation_scans', '-r', type=int, help='the number of scans to take in rotation axis')
-    parser.add_argument('--curve_scans', '-c', type=int, help='the number of scans to take in curve axis')
-    args = parser.parse_args()
-
-    if args.rotation_scans:
-        rotation_angle = 360 / args.rotation_scans
-        if not rotation_angle:
-            rotation_angle = 1
-    else:
-        rotation_angle = 360
-
-    if args.curve_scans:
-        curve_angle = 100 / args.curve_scans
-        if not curve_angle:
-            curve_angle = 1
-    else:
-        curve_angle = 100
-
+    rotation_angle, curve_angle = parse_arguments()
+    
     rotations = [int(x) for x in range(360) if x % rotation_angle == 0]
     curves = [int(x) for x in range(100) if x % curve_angle == 0]
 
